17a240728ca2e7506aa21dac0dea5531ca2afdae
Cintrian <velegoj2@yandex.ru>
2022-08-26 00:31:05 +0300

    task 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        var firstBoxerWeight = 78.2;
-        var secondBoxerWeight = 82.7;
-        var totalWeight = firstBoxerWeight + secondBoxerWeight;
-        System.out.println("Общий вес бойцов " + totalWeight + " кг");
-        var differenceWeigh = firstBoxerWeight - secondBoxerWeight;
-        System.out.println("Разница между весами бойцов " + differenceWeigh + " кг");
-        var firstMethod = secondBoxerWeight - firstBoxerWeight;
-        System.out.println("Разница между весами бойцов " + firstMethod + " кг");
-        var secondMethod = secondBoxerWeight % firstBoxerWeight;
-        System.out.println("Второй боец весит больше на " + secondMethod + " кг");
+       var workingHours = 640;
+       var personalWorkingHours = 8;
+       var workers = workingHours / personalWorkingHours;
+        System.out.println("Всего работников в компании " + workers + " человек");
+        workers = workers + 94;
+        workingHours = workers * personalWorkingHours;
+        System.out.println("Если в компании работает " + workers + " человека, то всего " + workingHours + " часа работы может быть поделено между сотрудниками");
 
 
     }
